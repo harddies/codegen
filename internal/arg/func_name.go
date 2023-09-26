7dc8685a66330ca4186f94a146db626eb7aff3f1
Ronnie <ronnie@RonniedeMac.local>
2023-09-26 11:24:55 +0800

    feat: page

--- internal/arg/func_name.go
+++ internal/arg/func_name.go
@@ -1,15 +1,9 @@
 package arg
 
-import "strings"
-
 func SetFuncNames(fns []string) Options {
 	return func(s *Sets) {
 		if len(fns) <= 0 {
 			return
-		}
-
-		for i := range fns {
-			fns[i] = strings.ToTitle(fns[i])
 		}
 		s.FuncNames = fns
 	}
